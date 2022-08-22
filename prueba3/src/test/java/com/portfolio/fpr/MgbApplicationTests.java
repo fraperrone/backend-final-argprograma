923245cdd007551e0a06b0fc3f1657d4abe9b390
fraperrone <fperronerey@gmail.com>
2022-08-22 08:48:51 -0300

    Add files via upload

---
+++ prueba3/src/test/java/com/portfolio/fpr/MgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.fpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
